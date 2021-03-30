ae76dcbb0728702ed7604c1319f95a979f0c1ba7
Maria Barrera <mbarrera_usa@yahoo.com>
2021-03-30 14:36:24 -0700

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -8,25 +8,25 @@
 // ---------------------------------------------------------------
 // Initialize display
 // ---------------------------------------------------------------
-function init (){
-  var selector = d3.selectAll('#selDataset');
+// function init (){
+//   var selector = d3.selectAll('#selDataset');
 
-  d3.json('samples.json').then((data)=>{
+//   d3.json('samples.json').then((data)=>{
   
-      var sampleDataNames=data.names;
-      sampleDataNames.forEach((sample) => {
-          selector
-            .append("option")
-            .text(sample)
-            .property("value", sample);
-          });
+//       var sampleDataNames=data.names;
+//       sampleDataNames.forEach((sample) => {
+//           selector
+//             .append("option")
+//             .text(sample)
+//             .property("value", sample);
+//           });
     
-      var defaultID = sampleDataNames[0];
+//       var defaultID = sampleDataNames[0];
   
-      barChart(defaultID);
+//       barChart(defaultID);
       
-    });
-   };
+//     });
+//    };
 // ----------------------------------------------------------------------------
 
 // ----------------------------------------------------------------------------
@@ -37,27 +37,26 @@ function init (){
 
       d3.json('samples.json').then((data)=>{
           var dataSamples = data.samples;
-          var ID = dataSamples.map(row=>row.id).indexOf(subjectId);
+          var sampleID = dataSamples.map(row=>row.id).indexOf(subjectId);
           var otuValueTopTen = dataSamples.map(row=>row.sample_values);
-          var otuValueTopTen = otuValueTopTen[ID].slice(0,10).reverse();
+          var otuValueTopTen = otuValueTopTen[sampleID].slice(0,10).reverse();
           var otuIdTopTen = dataSamples.map(row=>row.otu_ids);
-          var otuIdTopTen = otuIdTopTen[ID].slice(0,10);
-          var otuLabelTen = dataSamples.map(row=>row.otu_labels).slice(0,10);
+          var otuIdTopTen = otuIdTopTen[sampleID].slice(0,10);
+          var otuLabelTopTen = dataSamples.map(row=>row.otu_labels).slice(0,10);
           
           var trace={
               x: otuValueTopTen,
               y: otuIdTopTen.map(r=>`UTO ${r}`),
               text: otuLabelTopTen,
               type:'bar',
-              // orientation:'h'
+              orientation:'h'
           }
 
           // Define plot layout
           var layout = {
             title: "The TOP TEN OTUs",
             xaxis: { title: "Title" },
-            yaxis: { title: "OTUs"},
-            margin: {t: 40, l: 160}
+            yaxis: { title: "OTUs"}
           };
     
          Plotly.newPlot('bar',[trace],layout);
